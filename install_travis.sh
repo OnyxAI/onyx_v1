e7fe9d2f4c3d789274a47da2470c540e715adfde
Aituglo <ckhouani@live.fr>
2016-12-21 23:34:17 +0100

    travis

---
+++ install_travis.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+echo "Onyx Install"
+echo "Dependencies Install"
+echo "***"
+apt-get --assume-yes update
+apt-get --assume-yes install build-essential python-software-properties python-setuptools python-dev python-virtualenv curl git git-core openssl libssl-dev memcached python-memcache libmemcached-dev zlib1g-dev libffi-dev
+
