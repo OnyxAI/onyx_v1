ce8cd0c642b2f7a878533b53820d819a0247703e
Aituglo <ckhouani@live.fr>
2017-04-30 14:29:52 +0200

    Add loader and more speed

--- install_debian_script.sh
+++ install_debian_script.sh
@@ -9,4 +9,4 @@ echo "Onyx Install"
 echo "Dependencies Install"
 echo "***"
 apt-get --assume-yes update
-apt-get --assume-yes install build-essential python python-babel python-software-properties python-pip python-setuptools python-dev python-virtualenv openssl libssl-dev memcached python-memcache libmemcached-dev zlib1g-dev libffi-dev swig3.0 swig python-pygame python-pyaudio python3-pyaudio sox libatlas-base-dev libportaudio2 libportaudiocpp0 portaudio19-dev flac mplayer mpg321
+apt-get --assume-yes install git build-essential python python-babel python-software-properties python-pip python-setuptools python-dev python-virtualenv openssl libssl-dev memcached python-memcache libmemcached-dev zlib1g-dev libffi-dev swig3.0 swig python-pygame python-pyaudio python3-pyaudio sox libatlas-base-dev libportaudio2 libportaudiocpp0 portaudio19-dev flac mplayer mpg321
