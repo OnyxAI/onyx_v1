26799210dec16c3786a2c8161eb75ee0166684ce
Aituglo <ckhouani@live.fr>
2017-04-30 19:24:29 +0200

    Add gunicorn

---
+++ onyx/wsgi.py
@@ -0,0 +1,22 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Onyx Project
+http://onyxproject.fr
+Software under licence Creative Commons 3.0 France
+http://creativecommons.org/licenses/by-nc-sa/3.0/fr/
+You may not use this software for commercial purposes.
+@author :: Cassim Khouani
+"""
+from onyx import *
+from werkzeug.contrib.fixers import ProxyFix
+
+app = create_app()
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
+blueprints_fabrics(app, get_blueprints(app))
+error_pages(app, get_blueprint_name(app))
+
+if __name__ == "__main__":
+    app.run()
