9ab54d251ff337b79be8dbcbab3f69716bb0195f
Aituglo <ckhouani@live.fr>
2017-06-04 00:29:04 +0200

    Travis fix

--- run_travis.sh
+++ run_travis.sh
@@ -1,2 +1,31 @@
 #!/usr/bin/env bash
+TOP=$(cd $(dirname $0) && pwd -L)
+
+if [ ! -f ${TOP}/onyx/app_config.py ]; then
+    echo "App Config not found!"
+    echo "Create App Config"
+    cp ${TOP}/onyx/config_example.py ${TOP}/onyx/app_config.py
+fi
+
+if [ ! -f ${TOP}/onyx/config/onyx.cfg ]; then
+    echo "Config not found!"
+    echo "Create Config"
+    cp ${TOP}/onyx/config/onyx_example.cfg ${TOP}/onyx/config/onyx.cfg
+fi
+
+make compilelang
+
+arch=$(arch)
+os=$(uname -s)
+
+cd ${TOP}/onyx/client/speech/assets
+
+if [ ! -f ${TOP}/onyx/client/speech/assets/_snowboydetect.so ] || [ ! -f ${TOP}/onyx/client/speech/assets/snowboydetect.py ]; then
+    echo "Snowboy not found not found!"
+    echo "Download Snowboy"
+    wget http://download.onyxlabs.fr/snowboy/snowboy_${os}_${arch}.tar.gz
+    tar zxvf snowboy_${os}_${arch}.tar.gz
+    rm snowboy_${os}_${arch}.tar.gz
+fi
+
 py.test tests/
