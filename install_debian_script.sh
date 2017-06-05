0b58246ae9279a1d8a3f96d25b8040b48cd6c3f6
Aituglo <ckhouani@live.fr>
2017-06-05 15:06:53 +0200

    change setup

--- install_debian_script.sh
+++ install_debian_script.sh
@@ -9,4 +9,4 @@ echo "Onyx Install"
 echo "Dependencies Install"
 echo "***"
 apt-get --assume-yes update
-apt-get --assume-yes install git jq build-essential python screen python-babel python-software-properties python-pip python-setuptools python-dev python-virtualenv openssl libssl-dev memcached python-memcache libmemcached-dev zlib1g-dev libffi-dev swig3.0 swig python-pygame sox python3-pyaudio libatlas-base-dev libportaudio2 libportaudiocpp0 portaudio19-dev flac mplayer mpg321
+apt-get --assume-yes install git jq dnsmasq build-essential python screen python-babel python-software-properties python-pip python-setuptools python-dev python-virtualenv openssl libssl-dev memcached python-memcache libmemcached-dev zlib1g-dev libffi-dev swig3.0 swig python-pygame sox python3-pyaudio libatlas-base-dev libportaudio2 libportaudiocpp0 portaudio19-dev flac mplayer mpg321
