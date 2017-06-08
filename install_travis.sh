a5da308301c5c7fdf5d7d20dd8a82d91bec3c61c
Aituglo <ckhouani@live.fr>
2017-06-08 16:37:13 +0200

    Fix travis

--- install_travis.sh
+++ install_travis.sh
@@ -9,4 +9,33 @@ echo "Onyx Install"
 echo "Dependencies Install"
 echo "***"
 apt-get --assume-yes update
-apt-get --assume-yes install build-essential python python-software-properties python-pip python-setuptools python-dev python-virtualenv openssl libssl-dev memcached python-memcache libmemcached-dev zlib1g-dev libffi-dev 
+apt-get --assume-yes install build-essential python python-software-properties python-pip python-setuptools python-dev python-virtualenv openssl libssl-dev memcached python-memcache libmemcached-dev zlib1g-dev libffi-dev
+
+TOP=$(cd $(dirname $0) && pwd -L)
+
+if [ ! -f ${TOP}/onyx/app_config.py ]; then
+    echo "App Config not found!"
+    echo "Create App Config"
+    cp ${TOP}/onyx/config_example.py ${TOP}/onyx/app_config.py
+fi
+
+if [ ! -f ${TOP}/onyx/config/onyx.cfg ]; then
+    echo "Config not found!"
+    echo "Create Config"
+    cp ${TOP}/onyx/config/onyx_example.cfg ${TOP}/onyx/config/onyx.cfg
+fi
+
+make compilelang
+
+arch=$(arch)
+os=$(uname -s)
+
+cd ${TOP}/onyx/client/speech/assets
+
+if [ ! -f ${TOP}/onyx/client/speech/assets/_snowboydetect.so ] || [ ! -f ${TOP}/onyx/client/speech/assets/snowboydetect.py ]; then
+    echo "Snowboy not found not found!"
+    echo "Download Snowboy"
+    wget http://download.onyxlabs.fr/snowboy/snowboy_${os}_${arch}.tar.gz
+    tar zxvf snowboy_${os}_${arch}.tar.gz
+    rm snowboy_${os}_${arch}.tar.gz
+fi
