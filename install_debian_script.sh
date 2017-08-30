561f4ebef1c388036898edbe1e3229556013c016
Aituglo <ckhouani@live.fr>
2017-08-30 00:51:11 +0000

    change

--- install_debian_script.sh
+++ install_debian_script.sh
@@ -15,7 +15,7 @@ apt-get -y install nodejs npm
 ln -s /usr/bin/nodejs /usr/bin/node
 npm install -g pm2
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
-echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/3.4 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
-apt-get -y update
-apt-get install -y mongodb-org
+#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+#echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/3.4 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+#apt-get -y update
+#apt-get install -y mongodb-org
